c890b884f26d360750e8383c3225dac3e8a2a2c1
sheikshammadh <sheikshammadh18@gmail.com>
2025-05-03 17:53:06 +0530

    sn

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import User from './Props/User';
 import Message from './state.example/Msg';
 import Logform from './state.example/btn';
+import Forms from './forms/form';
 function App() {
     return (
         <div>
           <h3>App components</h3>
           <hr />
-          <><User/><Message/><Logform/></>
-          
+          <><User/><Message/><Logform/><Forms/></>
         </div>
     )
 }
